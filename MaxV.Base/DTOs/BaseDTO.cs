faf74ab868e4d5450e77e6d59ea5e23fd669ca00
Vinh Le <vinh.le@infodation.vn>
2021-06-06 14:29:30 +0700

    update BaseDTO

--- MaxV.Base/DTOs/BaseDTO.cs
+++ MaxV.Base/DTOs/BaseDTO.cs
@@ -8,10 +8,5 @@ namespace MaxV.Base.DTOs
     {
         public TKey Id { get; set; }
         public Guid Uuid { get; set; }
-        public DateTime? CreateAt { get; set; }
-        public DateTime? UpdateAt { get; set; }
-        public string CreateBy { get; set; }
-        public string UpdateBy { get; set; }
-        public long? Deleted { get; set; }
     }
 }
